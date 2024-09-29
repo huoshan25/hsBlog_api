54f77d3c02d68de0d185a9dd770c3a11902f450c
cainiao25 <116700226+cainiao25@users.noreply.github.com>
2024-09-29 15:08:35 +0800

    feat: 全局管道验证（ValidationPipe）

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { CategoryService } from "src/modules/category/category.service";
 import { CustomHttpExceptionFilter } from './common/error2.interceptor';
 import { TransformInterceptor } from './core/transform.interceptor';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 注入CategoriesService并调用seedDefaultCategories方法
   const categoriesService = app.get(CategoryService);
   await categoriesService.seedDefaultCategories();
-
+  /*全局验证管道：*/
+  app.useGlobalPipes(new ValidationPipe());
   /**把自定义过滤器应用为全局过滤器*/
   app.useGlobalFilters(new CustomHttpExceptionFilter());
   /*统一响应*/
