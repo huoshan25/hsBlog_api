bad970638060707029afe6b7bd4599ac1d00739c
cainiao25 <116700226+cainiao25@users.noreply.github.com>
2024-09-29 15:08:07 +0800

    feat: 数据库配置抽离

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,38 +2,31 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
-import { User } from './modules/user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
-import { Article } from "./modules/article/entities/article.entity";
 import { CategoryModule } from './modules/category/category.module';
 import { ArticleModule } from './modules/article/article.module'
-import { Category } from "./modules/category/entities/category.entity";
 import { ErrorInterceptor } from "./common/error.interceptor";
 import { ResponseInterceptor } from './common/response.interceptor';
 import { OssModule } from './modules/oss/oss.module';
-import { Tag } from './modules/article/entities/tag.entity';
 import { AuthMiddleware } from './middleware/auth.middleware';
 import { authConfig } from './config/auth.config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import databaseConfig from './config/database.config';
 
 @Module({
   imports: [
-    UserModule,
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "localhost",
-      port: 3306,
-      username: "root",
-      password: "123456",
-      database: "blog",
-      synchronize: true,
-      logging: true,
-      entities: [User, Article, Category, Tag],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugin: 'sha256_password',
-      }
+    ConfigModule.forRoot({
+      isGlobal: true, // 使 ConfigModule 在全局可用
+      envFilePath: `.env.${process.env.NODE_ENV}`, // 指定 .env 文件的路径
+      load: [databaseConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+      inject: [ConfigService],
     }),
     JwtModule.register({
       /**声明为全局模块*/
@@ -44,6 +37,7 @@ import { authConfig } from './config/auth.config';
     CategoryModule,
     ArticleModule,
     OssModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, ErrorInterceptor, ResponseInterceptor, AuthMiddleware],
---
+++ src/config/database.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'mysql',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '123456',
+  database: process.env.DB_NAME || 'blog',
+  synchronize: process.env.DB_SYNC === 'true',
+  logging: process.env.DB_LOGGING === 'true',
+  entities: ['dist/modules/**/*.entity{.ts,.js}'], // 使用双星号来匹配任意深度的子目录
+  poolSize: parseInt(process.env.DB_POOL_SIZE, 10) || 10,
+  connectorPackage: 'mysql2',
+  extra: {
+    authPlugin: 'sha256_password',
+  }
+}));
